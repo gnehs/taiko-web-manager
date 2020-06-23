c48710bc16ee313d54cf023a1d366fb36195fa68
gnehs <jayuiop@gmail.com>
2020-06-23 23:28:38 +0800

    update Category schema

--- server/db/categories.js
+++ server/db/categories.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Category = new Schema({
     id: Number,
-    title: String
+    title: String,
+    //aliases: null,
+    song_skin: {
+        "background": String,
+        "bg_img": String,
+        "border": [String, String],
+        "info_fill": String,
+        "outline": String,
+        "sort": Number
+    },
+    "title": String,
+    "title_lang":
+    {
+        "cn": String,
+        "en": String,
+        "ja": String,
+        "ko": String,
+        "tw": String
+    }
 }, { collection: 'categories' });
 const model = mongoose.model('category', Category)
 async function getCategoriesList() {
